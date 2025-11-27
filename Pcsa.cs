498b656f4993a97784cca51ffbd1a9d1837d81b2
Opilane TTHK <opilane@tthk.ee>
2025-11-27 09:49:06 +0200

    Vipalgrad

---
+++ Pcsa.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("for loop");
+            int loop = 10;
+            for (int i = 0; i < loop; i++)
+            {
+                Console.WriteLine("Loop" + i);
+                Console.Beep();
+            }
+        }
+    }
+}
