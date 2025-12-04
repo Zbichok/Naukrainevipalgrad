d25fd183683b2b7608b1955bb97c7147229762ea
Opilane TTHK <opilane@tthk.ee>
2025-12-04 11:45:04 +0200

    dsa

---
+++ cheto.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Täisarvude kokku liitmine kuni kümneni");
+            Console.WriteLine("---------------------------------------");
+
+            int summa = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                summa += i; 
+                Console.WriteLine("Summa on : " + summa);
+            }
+
+        }
+    }
+}
