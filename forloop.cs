5729b4e5ff650a8da6680180473010e035c816f4
Opilane TTHK <opilane@tthk.ee>
2025-12-04 09:39:10 +0200

    Forloop thx

---
+++ forloop.cs
@@ -0,0 +1,24 @@
+﻿namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ushort i = 0;
+            for (; ; )
+            {
+                Console.WriteLine("Rida number: " + i);
+                i++;
+
+                if (i == 11)
+                {
+                    break; 
+                }
+            }
+
+
+        }
+    }
+    }
+
