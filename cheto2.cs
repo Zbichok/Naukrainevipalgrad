c8c553b7c2826c53c4dc3ea3492f3c9b30a42519
Opilane TTHK <opilane@tthk.ee>
2025-12-11 10:42:30 +0200

    Create cheto2.cs

---
+++ cheto2.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("korrustustabel");
+
+            int j, i, n;
+
+            Console.WriteLine("sisesta tabelisse number, millest soovid alustada");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("korrutustabel ühest kuni {0}", n);
+            Console.WriteLine("\n");
+
+            for (i = 1; i < 10; i++)
+            {
+                for (j = 1; j <= n; j++)
+                {
+                    Console.WriteLine("{0}x{1} = {2},", j, i, i * j);
+                }
+            }
+        }
+    }
+}
